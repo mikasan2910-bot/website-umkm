4abe1522adaa93549dde6a5427bdbfa18d849463
Mikail Sani <mikasan2910@gmail.com>
2026-09-20 17:18:35 +0700

    fix: rapikan struktur dan siapkan GitHub Pages

--- css/js/css/js/js/script.js
+++
@@ -1,6 +0,0 @@
-const promoButton = document.querrySelector("#PromoButton");
-
-PromoButton.addEventListener("click", () => {
-    promoButton.textContent = "Promo: Beli 2 gratis tester!";
-    console.log("Promo Kopi Bangsa berhasil ditampilkan.");
-});
