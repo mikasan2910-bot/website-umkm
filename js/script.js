b40722191f58fd60db01ff13533d78f966866d6d
Mikail Sani <mikasan2910@gmail.com>
2026-09-20 16:50:51 +0700

    feat: tambah kontak singkat dan semantic html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", () => {
+    promoButton.textContent = "Promo warga kedai: beli 2 kopi, gratis tester kopi lokal!";
+    console.log("Promo Kopi Bangsa siap dinikmati pelanggan.");
+});
